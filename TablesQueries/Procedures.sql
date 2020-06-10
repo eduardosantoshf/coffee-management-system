993e9691b32d78dca6cdf4d12cb53cf29801c54c
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-10 21:44:03 +0100

    Trigger checkEmpregado working with a bug

--- TablesQueries/Procedures.sql
+++ TablesQueries/Procedures.sql
@@ -140,9 +140,32 @@ GO
 --EXEC getAlmocos;
 --EXEC getPasteis;
 
-CREATE TRIGGER [dbo].[checkEmpregado] BEFORE INSERT ON Cafes.Pessoa
-FOR EACH ROW
+/*
+ALTER TRIGGER Cafes.checkEmpregado ON Cafes.Empregado
+INSTEAD OF INSERT
+AS
 	BEGIN
+		DECLARE @NIF INT;
+		DECLARE @NIF_cafe INT;
+		DECLARE @idade INT;
+		DECLARE @nome VARCHAR(30);
+		DECLARE @data_inic_contrato DATE;
+		SELECT @NIF = NIF, @nome = nome, @NIF_cafe = NIF_cafe, @idade = idade, @nome = nome, @data_inic_contrato = data_inic_contrato  FROM inserted;
+		IF ([dbo].[checkPessoa](@NIF, @nome) = 1)
+			RAISERROR('Já existe',16,1);
+		ELSE
+			INSERT INTO Cafes.Pessoa([NIF], [nome]) VALUES (@NIF, @nome);
+			INSERT INTO Cafes.Empregado([NIF], [NIF_cafe], [idade], [nome], [data_inic_contrato]) VALUES (@NIF, @NIF_cafe, @idade, @nome, @data_inic_contrato);
+	END
+GO
+*/
+--DROP TRIGGER Cafes.checkEmpregado;
+
+INSERT INTO Cafes.Empregado([NIF],[NIF_cafe],[idade],[nome],[data_inic_contrato]) VALUES(241055237,211327138,81,'Winifred Walter','2016-01-20')
+
+		
+			
+			
 		
 
 
