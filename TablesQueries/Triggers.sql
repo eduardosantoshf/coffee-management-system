9fc60514f1a9bcb981d92c2eef0f494d748a9c34
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-10 22:26:21 +0100

    Created Triggers' file

---
+++ TablesQueries/Triggers.sql
@@ -0,0 +1,21 @@
+/*
+CREATE TRIGGER Cafes.checkEmpregado ON Cafes.Empregado
+INSTEAD OF INSERT
+AS
+	BEGIN
+		DECLARE @NIF INT;
+		DECLARE @NIF_cafe INT;
+		DECLARE @idade INT;
+		DECLARE @nome VARCHAR(30);
+		DECLARE @data_inic_contrato DATE;
+		SELECT @NIF = NIF, @nome = nome, @NIF_cafe = NIF_cafe, @idade = idade, @nome = nome, @data_inic_contrato = data_inic_contrato  FROM inserted;
+		IF ([dbo].[checkPessoa](@NIF, @nome) = 1)
+			RAISERROR('Já existe',16,1);
+		ELSE
+			BEGIN
+				INSERT INTO Cafes.Pessoa([NIF], [nome]) VALUES (@NIF, @nome);
+				INSERT INTO Cafes.Empregado([NIF], [NIF_cafe], [idade], [nome], [data_inic_contrato]) VALUES (@NIF, @NIF_cafe, @idade, @nome, @data_inic_contrato);
+			END
+	END
+GO
+*/
