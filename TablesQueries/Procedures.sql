4ad785412ba90f9e2ce64579dd80093b94662bda
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-10 02:19:58 +0100

    Added procedure InsertCompra

--- TablesQueries/Procedures.sql
+++ TablesQueries/Procedures.sql
@@ -1,8 +1,37 @@
 /*
+ALTER PROCEDURE [dbo].[insertAdministrador](@username VARCHAR(30), @pwd VARCHAR(30))
+AS
+BEGIN
+	INSERT INTO Cafes.Administrador([username],[pwd]) VALUES (@username, HASHBYTES('MD5', @pwd));
+END;
+*/
+
+/*
+CREATE PROCEDURE [dbo].[verifyLogin](@username VARCHAR(30), @pwd VARCHAR(30), @flag INT OUTPUT)
+AS
+BEGIN
+	DECLARE @temp_pwd VARBINARY(36);
+	SET @temp_pwd = (SELECT pwd FROM Cafes.Administrador)
+	IF (HASHBYTES('MD5', @pwd) = @temp_pwd)
+		SET @flag = 1
+	ELSE
+		SET @flag = 0
+END
+*/
+
+--EXEC insertAdministrador 'administrador', 'palavrachave';
+/*
+DECLARE @flag2 INT;
+EXEC verifyLogin 'ola', 'palavrachave', @flag2 OUTPUT; --Should return 1
+PRINT @flag2;
+*/
+
+
+/*
 CREATE PROCEDURE [dbo].[insertRecibo](@ClienteNIF INT, @EmpNIF INT, @data_recibo DATE, @valor FLOAT)
 AS
 BEGIN 
-	INSERT INTO Cafes.recibo(ClienteNIF, EmpNIF, data_recibo, valor)
+	INSERT INTO Cafes.Recibo(ClienteNIF, EmpNIF, data_recibo, valor)
 	SELECT @ClienteNIF, @EmpNIF, cast(@data_recibo as date), @valor
 END
 
@@ -28,20 +57,20 @@ BEGIN
 	INSERT INTO Cafes.Produto(nomeP, precoP, tipoP)
 	SELECT @nomeP, @precoP, @tipoP
 END
-
+*/
 --EXEC addProduto café, 0.65, 1;
 --SELECT * FROM Cafes.Produto;
-
+/*
 GO
 CREATE PROCEDURE [dbo].[removeProduto](@pID INT)
 AS
 BEGIN
 	DELETE FROM Cafes.Produto WHERE ID_P = @pID
 END
-
---EXEC removeProduto 1;
+*/
+--EXEC removeProduto 3;
 --SELECT * FROM Cafes.Produto;
-
+/*
 
 --This selects the last (max) ID from products' table
 --SELECT MAX(ID_P) FROM Cafes.Produto;
@@ -63,31 +92,16 @@ PRINT @lastID;
 
 
 --EXEC getLastReciboID
-
-
-ALTER PROCEDURE [dbo].[insertAdministrador](@username VARCHAR(30), @pwd VARCHAR(30))
-AS
-BEGIN
-	INSERT INTO Cafes.Administrador([username],[pwd]) VALUES (@username, HASHBYTES('MD5', @pwd));
-END;
 */
-
-
-
 /*
-CREATE PROCEDURE [dbo].[verifyLogin](@username VARCHAR(30), @pwd VARCHAR(30), @flag INT OUTPUT)
+CREATE PROCEDURE [dbo].[insertCompra](@reciboID INT, @produtoID INT)
 AS
 BEGIN
-	DECLARE @temp_pwd VARBINARY(36);
-	SET @temp_pwd = (SELECT pwd FROM Cafes.Administrador)
-	IF (HASHBYTES('MD5', @pwd) = @temp_pwd)
-		SET @flag = 1
-	ELSE
-		SET @flag = 0
+	INSERT INTO Cafes.Compra([Recibo_ID],[Produto_ID]) VALUES (@reciboID, @produtoID);
 END
 */
+--SELECT * FROM Cafes.Recibo;
+--SELECT * FROM Cafes.Produto;
+--EXEC insertCompra 1, 2;
+SELECT * FROM Cafes.Compra;
 
---EXEC insertAdministrador 'administrador', 'palavrachave';
-DECLARE @flag2 INT;
-EXEC verifyLogin 'ola', 'palavrachave', @flag2 OUTPUT; --Should return 1
-PRINT @flag2;
