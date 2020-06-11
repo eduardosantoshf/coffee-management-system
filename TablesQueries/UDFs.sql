3f9655bd12aabe4f2190635eec03601f7b6a5646
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-11 20:09:49 +0100

    Minor fix

--- TablesQueries/UDFs.sql
+++ TablesQueries/UDFs.sql
@@ -8,28 +8,24 @@ AS
 	END
 GO
 
-*/
+
 ALTER FUNCTION [dbo].[checkQuantidadeProduto](@Produto_ID INT, @Recibo_ID INT) RETURNS INT
 AS
 	BEGIN
-		IF EXISTS(SELECT * FROM Cafes.Compra WHERE ([Produto_ID] = @Produto_ID))
+		IF EXISTS(SELECT * FROM Cafes.Compra WHERE (([Produto_ID] = @Produto_ID) AND [Recibo_ID] = @Recibo_ID))
 			RETURN 1;
 		RETURN 0;
 	END
 GO
-/*
 
-
-CREATE FUNCTION [dbo].[getProdutoQuantidade](@ID_P INT) RETURNS INT
+CREATE FUNCTION [dbo].[getProdutoQuantidade](@Produto_ID INT, @Recibo_ID INT) RETURNS INT
 AS
 	BEGIN
 		DECLARE @quantidade INT;
-		SET @quantidade = (SELECT [quantidade] FROM Cafes.Compra WHERE [Produto_ID] = @ID_P);
+		SET @quantidade = (SELECT [quantidade] FROM Cafes.Compra WHERE (([Produto_ID] = @Produto_ID) AND [Recibo_ID] = @Recibo_ID));
 		RETURN @quantidade;
 	END
 GO
-
-
 
 ALTER FUNCTION [dbo].[checkReciboInCompra](@Recibo_ID INT) RETURNS INT
 AS
