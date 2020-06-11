5db3c0e22cfa59fb6efe0179e02305b13ec7050a
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-11 22:42:08 +0100

    Final

--- TablesQueries/InsertCafes.sql
+++ TablesQueries/InsertCafes.sql
@@ -1,17 +1,17 @@
---INSERT INTO Cafes.Cafe(NIF,nome,morada) VALUES(411766585,'dictum. Phasellus','375-9706 Metus. Rd.'),(530435413,'sollicitudin a,','2807 Nisi. Street'),(577791382,'nisi a','4722 Quis, Rd.'),(658325127,'neque. Sed','379-6870 Ornare St.'),(681397292,'Sed dictum.','4919 Elit Rd.'),(726903330,'at auctor','370-2615 Nunc Road'),(742894911,'ut odio','Ap #658-7265 Purus Avenue'),(753654993,'nec, diam.','Ap #597-6014 Eu St.'),(379288174,'ut, nulla.','P.O. Box 617, 1146 Ipsum Rd.'),(860598746,'aliquam eu,','888-6584 Dolor. Ave'),(892065174,'risus. Morbi','Ap #612-6042 Arcu Rd.');
---INSERT INTO Cafes.Cafe([NIF],[nome],[morada]) VALUES(619248943,'vitae, sodales','Ap #907-404 Velit. St.'),(942488398,'enim. Curabitur','426-2484 Leo. St.'),(583940249,'sollicitudin orci','Ap #659-6930 Nulla. Avenue'),(798813425,'dui, semper','839-3111 Fames Street'),(653133221,'et magnis','Ap #212-5157 Congue St.'),(211327138,'Quisque ornare','456-2484 Leo. St.'),(789830372,'lobortis augue','508-4022 Metus Street'),(723791278,'Nulla semper','Ap #307-404 Velit. St.'),(719581035,'ante lectus','Ap #367-404 Velit. St.'),(358222851,'imperdiet ornare.','208-4022 Metus Street');
---INSERT INTO Cafes.Cafe([NIF],[nome],[morada]) VALUES(862486192,'mattis ornare,','Ap #903-404 Velit. St.'),(971418125,'nunc sed','148-1773 Interdum. St.'),(694977571,'nec tempus','9283 Non, Avenue'),(248747427,'vel turpis.','Ap #897-9068 Sed St.'),(183370700,'egestas. Aliquam','927 Scelerisque Avenue'),(530245861,'Ut tincidunt','Ap #863-8812 Urna Av.'),(813177521,'orci. Ut','7157 Eros Rd.'),(427977540,'tempus risus.','243-4022 Mario Street'),(472192179,'purus, in','Ap #707-404 Velit. St.'),(998565502,'Nullam nisl.','508-302 Metus Street');
+INSERT INTO Cafes.Cafe(NIF,nome,morada) VALUES(411766585,'E-Cafe','Rua da Buba');
 
---INSERT INTO Cafes.Cafe_Bar(NIF,nome,morada) VALUES(411766585,'dictum. Phasellus','375-9706 Metus. Rd.'),(530435413,'sollicitudin a,','2807 Nisi. Street'),(577791382,'nisi a','4722 Quis, Rd.'),(658325127,'neque. Sed','379-6870 Ornare St.'),(681397292,'Sed dictum.','4919 Elit Rd.'),(726903330,'at auctor','370-2615 Nunc Road'),(742894911,'ut odio','Ap #658-7265 Purus Avenue'),(753654993,'nec, diam.','Ap #597-6014 Eu St.'),(379288174,'ut, nulla.','P.O. Box 617, 1146 Ipsum Rd.'),(860598746,'aliquam eu,','888-6584 Dolor. Ave'),(892065174,'risus. Morbi','Ap #612-6042 Arcu Rd.');
---INSERT INTO Cafes.Cafe_Pastelaria(NIF,nome,morada,no_bolos) VALUES(619248943,'mi lacinia','101 Ligula. Avenue',98),(942488398,'urna. Nunc','3913 Dolor Street',132),(583940249,'risus quis','4341 Quam Avenue',113),(798813425,'aliquet nec,','894-8598 Nibh Rd.',114),(653133221,'at lacus.','Ap #618-483 Faucibus. St.',99),(211327138,'Integer urna.','878-1856 Risus. Rd.',29),(789830372,'Maecenas ornare','6431 Erat. Street',115),(723791278,'lacinia vitae,','P.O. Box 678, 4459 Lorem, Rd.',113),(719581035,'at risus.','671-2398 Orci. Street',131),(358222851,'enim. Mauris','506-3357 Enim Avenue',58);
---INSERT INTO Cafes.Cafe_Restaurante([NIF],[nome],[morada],[no_almocos]) VALUES(862486192,'mattis ornare,','Ap #903-404 Velit. St.',132),(971418125,'nunc sed','148-1773 Interdum. St.',194),(694977571,'nec tempus','9283 Non, Avenue',101),(248747427,'vel turpis.','Ap #897-9068 Sed St.',112),(183370700,'egestas. Aliquam','927 Scelerisque Avenue',134),(530245861,'Ut tincidunt','Ap #863-8812 Urna Av.',146),(813177521,'orci. Ut','7157 Eros Rd.',165),(427977540,'tempus risus.','243-4022 Mario Street',184),(472192179,'purus, in','Ap #707-404 Velit. St.',192),(998565502,'Nullam nisl.','508-302 Metus Street',179);
+INSERT INTO Cafes.Cafe_Bar(NIF,nome,morada) VALUES(411766585,'E-Cafe','Rua da Buba');
+INSERT INTO Cafes.Cafe_Pastelaria(NIF,nome,morada) VALUES(411766585,'E-Cafe','Rua da Buba');
+INSERT INTO Cafes.Cafe_Restaurante([NIF],[nome],[morada]) VALUES(411766585,'E-Cafe','Rua da Buba');
 
---INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(296969668,'Angelica Maldonado'),(132063497,'Megan Wise'),(687643810,'Tate Stout'),(723657350,'Magee Carey'),(236696799,'Axel Lowe'),(497784251,'Noelani Freeman'),(443138848,'Connor Brady'),(547916021,'Branden Cherry'),(142550724,'Xenos Brady'),(231915681,'Haviva Holloway');
---INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(241045237,'Winifred Walter'),(259143494,'Uma Mason'),(293595015,'Quentin Santana'),(810577576,'Avye Hancock'),(452467352,'Price Whitfield'),(896728691,'Chiquita Head'),(918313660,'Demetria Erickson'),(770167696,'Hadassah Mcdonald'),(481192418,'Arthur Greene'),(802270517,'Iliana Ballard');
---INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(379923298,'Anjolie Peck'),(508174850,'Medge Trevino'),(884448426,'Gay Bond'),(608068232,'Martin Estrada'),(353381104,'Frances Douglas'),(369437051,'Leroy Everett'),(568333318,'Candace Case'),(477154900,'Honorato Crawford'),(797610436,'Sara Wilkins'),(899461477,'Gary Roy');
---INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(513196871,'Jared Berg'),(321972703,'Jacob Guy'),(900541552,'Walker Fox'),(369024440,'Bruce Hines'),(593505493,'Shad Gill'),(766186809,'Joan Guthrie'),(225537635,'Fuller Glass'),(949597097,'Libby Francis'),(268189127,'Dylan Maynard'),(323856495,'Aaron Valdez');
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(296969668,'Angelica Maldonado'),(132063497,'Megan Wise'),(687643810,'Tate Stout'),(723657350,'Magee Carey'),(236696799,'Axel Lowe'),(497784251,'Noelani Freeman'),(443138848,'Connor Brady'),(547916021,'Branden Cherry'),(142550724,'Xenos Brady'),(231915681,'Haviva Holloway');
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(241045237,'Winifred Walter'),(259143494,'Uma Mason'),(293595015,'Quentin Santana'),(810577576,'Avye Hancock'),(452467352,'Price Whitfield'),(896728691,'Chiquita Head'),(918313660,'Demetria Erickson'),(770167696,'Hadassah Mcdonald'),(481192418,'Arthur Greene'),(802270517,'Iliana Ballard');
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(379923298,'Anjolie Peck'),(508174850,'Medge Trevino'),(884448426,'Gay Bond'),(608068232,'Martin Estrada'),(353381104,'Frances Douglas'),(369437051,'Leroy Everett'),(568333318,'Candace Case'),(477154900,'Honorato Crawford'),(797610436,'Sara Wilkins'),(899461477,'Gary Roy');
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(513196871,'Jared Berg'),(321972703,'Jacob Guy'),(900541552,'Walker Fox'),(369024440,'Bruce Hines'),(593505493,'Shad Gill'),(766186809,'Joan Guthrie'),(225537635,'Fuller Glass'),(949597097,'Libby Francis'),(268189127,'Dylan Maynard'),(323856495,'Aaron Valdez');
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(513696290,'John Franklin'),(235889530,'Iris Greene'),(799631594,'Alana Tyson'),(429529859,'Roth Holcomb'),(894940017,'Cameron Nieves'),(294939603,'Chastity Gay'),(629328443,'Beau Phillips'),(332426721,'Ginger Mclaughlin'),(874054574,'Medge Frost'),(455491867,'Yvonne Lowery');
 
---INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(513696290,'John Franklin'),(235889530,'Iris Greene'),(799631594,'Alana Tyson'),(429529859,'Roth Holcomb'),(894940017,'Cameron Nieves'),(294939603,'Chastity Gay'),(629328443,'Beau Phillips'),(332426721,'Ginger Mclaughlin'),(874054574,'Medge Frost'),(455491867,'Yvonne Lowery');
+--Admins
+INSERT INTO Cafes.Pessoa([NIF],[nome]) VALUES(820473910,'Joaquim Freitas'),(836401127,'Diogo Moreira'),(806344901,'Eduardo Santos'),(322850048,'Professor');
 
 --INSERT INTO Cafes.Cliente([NIF],[nome],[no_mesa]) VALUES(296969668,'Angelica Maldonado',14),(132063497,'Megan Wise',46),(687643810,'Tate Stout',17),(723657350,'Magee Carey',9),(236696799,'Axel Lowe',18),(497784251,'Noelani Freeman',4),(443138848,'Connor Brady',7),(547916021,'Branden Cherry',5),(142550724,'Xenos Brady',38),(231915681,'Haviva Holloway',37);
 --INSERT INTO Cafes.Empregado([NIF],[NIF_cafe],[idade],[nome],[data_inic_contrato]) VALUES(241045237,211327138,81,'Winifred Walter','2016-01-20'),(259143494,211327138,38,'Uma Mason','2017-03-03'),(293595015,211327138,98,'Quentin Santana','2020-12-20'),(810577576,211327138,62,'Avye Hancock','2016-02-19'),(452467352,211327138,61,'Price Whitfield','2019-12-19'),(896728691,211327138,4,'Chiquita Head','2019-08-31'),(918313660,211327138,90,'Demetria Erickson','2018-07-15'),(770167696,211327138,80,'Hadassah Mcdonald','2021-02-03'),(481192418,211327138,96,'Arthur Greene','2018-07-25'),(802270517,211327138,41,'Iliana Ballard','2016-08-24');
@@ -22,13 +22,13 @@
 
 
 --INSERT admins
-/*
-EXEC insertAdministrador 'admin1', 'palavrachave1';
-EXEC insertAdministrador 'admin2', 'palavrachave2';
-EXEC insertAdministrador 'admin3', 'palavrachave3';
-EXEC insertAdministrador 'admin4', 'palavrachave4';
-EXEC insertAdministrador 'admin5', 'palavrachave5';
-*/
+EXEC insertAdministrador 'profBD', 'BD2020', 322850048, 'Professor';
+EXEC insertAdministrador 'quimf', 'nice934', 820473910, 'Joaquim Freitas';
+EXEC insertAdministrador 'didi', '123', 836401127, 'Diogo Moreira';
+EXEC insertAdministrador 'dudu', '321', 806344901, 'Eduardo Santos';
+
+
+
 
 
 
