36f63c3f06e9dea8a2c2a125edbd8dd17bbae64c
Eduardo Santos <eduardosantoshf@gmail.com>
2020-06-11 01:17:37 +0100

    Changed procedure getProdutosInRecibo

--- TablesQueries/Procedures.sql
+++ TablesQueries/Procedures.sql
@@ -168,14 +168,14 @@ AS
 		INSERT INTO Cafes.Cliente([NIF], [nome]) VALUES (@NIF, @nome);
 	END
 GO
-*/
 
-CREATE PROCEDURE getProdutosInRecibo(@reciboID INT)
+
+ALTER PROCEDURE getProdutosInRecibo(@reciboID INT)
 AS
-	BEGIN
-		SELECT FROM Cafes.Compra WHERE reciboID = @reciboID;
-	END
+    BEGIN
+        Select Produto. * from Cafes.Compra join Cafes.Recibo on Cafes.Compra.Recibo_ID = Cafes.Recibo.reciboID Join Cafes.Produto on Cafes.Produto.ID_P = Cafes.Compra.Produto_ID where Cafes.Recibo.reciboID = @reciboID
+    END
 GO
-
+*/
 
 
